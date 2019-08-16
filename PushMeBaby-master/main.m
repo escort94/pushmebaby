c13ef87d4b5f2614fe398cf5b232d2d4c5ec13f0
escort94 <escort94@nate.com>
2019-08-16 17:30:58 +0900

    Update main.m

--- PushMeBaby-master/main.m
+++ PushMeBaby-master/main.m
@@ -5,7 +5,7 @@
 //  Created by Stefan Hafeneger on 07.04.09.
 //  Copyright __MyCompanyName__ 2009. All rights reserved.
 //
-
+// 20190816 edit
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[])
